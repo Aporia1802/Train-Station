9426844dd648f79a2d49de734eb55c0499c0ea4b
Công Hoàng <23664101.hoang@student.iuh.edu.vn>
2025-10-02 00:41:13 +0700

    Add library and loading_GUI

---
+++ src/database/ConnectDB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.*;
+
+/**
+ *
+ * @author CÔNG HOÀNG
+ */
+public class ConnectDB {
+    public static Connection conn = null;
+
+    public static void connect() throws SQLException {
+
+        String url = "jdbc:sqlserver://localhost:1433;databasename=QuanLyVeTau";
+        String user = "sa";
+        String password = "123456789"; 
+
+        conn = DriverManager.getConnection(url, user, password);
+
+    }
+
+    public static void disconnect() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+            }
+        }
+    }
+}
--- src/entity/HoaDon.java
+++ src/entity/HoaDon.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package gui;
+package entity;
 
 /**
  *
  * @author CÔNG HOÀNG
  */
-public class QuenMatKhau_GUI {
+public class HoaDon {
     
 }
