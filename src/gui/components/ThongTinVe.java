723d553b52b0143ee3539adbb17e846148e408db
Công Hoàng <23664101.hoang@student.iuh.edu.vn>
2025-12-22 07:08:39 +0700

    update

--- src/gui/components/ThongTinVe.java
+++ src/gui/components/ThongTinVe.java
@@ -381,9 +381,9 @@ public class ThongTinVe extends javax.swing.JPanel {
     double giam = 0;
 
     switch (loaiVe) {
-        case "Trẻ em" -> giam = 0.75;        // 50%
-        case "Sinh viên" -> giam = 0.9;     // 20%
-        case "Người cao tuổi" -> giam = 0.85; // 30%
+        case "Trẻ em" -> giam = 0.25;        
+        case "Sinh viên" -> giam = 0.1;    
+        case "Người cao tuổi" -> giam = 0.15; 
         default -> giam = 0;                // Người lớn
     }
 
